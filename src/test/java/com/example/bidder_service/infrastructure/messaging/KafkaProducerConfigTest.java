931de0d5d05f8cee8499ded470fecae71e0e281f
Danyelle Angelo <danyelle.angelo.tw@naturapay.net>
2025-10-02 22:12:34 -0300

    remove dependency

--- src/test/java/com/example/bidder_service/infrastructure/messaging/KafkaProducerConfigTest.java
+++ src/test/java/com/example/bidder_service/infrastructure/messaging/KafkaProducerConfigTest.java
@@ -5,7 +5,6 @@ import org.apache.kafka.common.serialization.StringSerializer;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Profile;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;

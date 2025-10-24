7b1d59ff4c764b1ee689f6f4e15adcad2ae52d90
Danyelle Angelo <danyelle.angelo.tw@naturapay.net>
2025-10-23 21:23:00 -0300

    adds logic to publish bid

--- src/main/java/com/example/bidder_service/application/BidService.java
+++ src/main/java/com/example/bidder_service/application/BidService.java
@@ -1,8 +1,39 @@
 package com.example.bidder_service.application;
 
 import com.example.bidder_service.domain.Bid;
+import com.example.bidder_service.domain.exception.BidProcessingException;
+import com.example.bidder_service.domain.exception.InfrastructureException;
+import com.example.bidder_service.domain.messaging.MessagePublisher;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
+@Component
 public class BidService {
-  public void publish(Bid bid) {
+  MessagePublisher publisher;
+  String target;
+  ObjectMapper mapper;
+
+  @Autowired
+  public BidService(MessagePublisher publisher, @Value("${messaging.targets.bid}") String target, ObjectMapper mapper) {
+    this.publisher = publisher;
+    this.target = target;
+    this.mapper = mapper;
+  }
+
+  public void publish(Bid bid) throws BidProcessingException, InfrastructureException {
+    String message = serialize(bid);
+
+    publisher.publish(target, message);
+  }
+
+  private String serialize(Bid bid) throws BidProcessingException {
+    try {
+      return mapper.writeValueAsString(bid);
+    } catch (JsonProcessingException e) {
+      throw new BidProcessingException("Failed to serialize bid", e);
+    }
   }
 }
--- src/test/java/com/example/bidder_service/application/BidServiceTest.java
+++ src/test/java/com/example/bidder_service/application/BidServiceTest.java
@@ -1,4 +1,55 @@
 package com.example.bidder_service.application;
 
+import com.example.bidder_service.domain.Bid;
+import com.example.bidder_service.domain.exception.BidProcessingException;
+import com.example.bidder_service.domain.exception.InfrastructureException;
+import com.example.bidder_service.domain.messaging.MessagePublisher;
+import com.example.bidder_service.factory.BidFactory;
+import com.example.bidder_service.infrastructure.messaging.KafkaProducerConfig;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
 public class BidServiceTest {
+  BidService service;
+  @Mock
+  MessagePublisher publisher;
+  private ObjectMapper mapper;
+
+  @BeforeEach
+  void setUp() {
+    mapper =  new ObjectMapper();
+    service = new BidService(publisher, "submitted-bids", mapper);
+  }
+
+  @Test
+  void shouldInvokePublisherWithMessageAndDestination() throws Exception {
+    Bid bid = BidFactory.validBid();
+    String expectedMessage = mapper.writeValueAsString(bid);
+
+    service.publish(bid);
+
+    verify(publisher, times(1)).publish("submitted-bids", expectedMessage);
+  }
+
+  @Test
+  void shouldThrowBidProcessingExceptionWhenSerializationFails() throws Exception  {
+    mapper = mock(ObjectMapper.class);
+    Bid bid = BidFactory.validBid();
+    when(mapper.writeValueAsString(bid)).thenThrow(new JsonProcessingException("fail")  {});
+
+    BidService service = new BidService(publisher, "submitted-bids", mapper);
+
+    assertThrows(BidProcessingException.class, () -> {
+      service.publish(bid);
+    });
+  }
 }

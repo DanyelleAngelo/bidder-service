5473d08cdf55aa3389dfa27a2bb047bcafe11662
Danyelle Angelo <danyelle.angelo.tw@naturapay.net>
2025-10-23 21:22:07 -0300

    adds exception for error processing bids

--- src/main/java/com/example/bidder_service/domain/exception/BidProcessingException.java
+++ src/main/java/com/example/bidder_service/domain/exception/BidProcessingException.java
@@ -1,14 +1,14 @@
 package com.example.bidder_service.domain.exception;
 
-public class InfrastructureException extends Exception {
-  private final ErrorCode errorCode = ErrorCode.INFRASTRUCTURE_EXCEPTION;
+public class BidProcessingException extends Exception {
+  private final ErrorCode errorCode = ErrorCode.BID_PROCESSING_EXCEPTION;
 
-  public InfrastructureException(String message, Throwable cause) {
-    super(String.format("%s: %s", ErrorCode.INFRASTRUCTURE_EXCEPTION, message), cause);
+  public BidProcessingException(String message, Throwable cause) {
+    super(String.format("%s: %s", ErrorCode.BID_PROCESSING_EXCEPTION, message), cause);
   }
 
-  public InfrastructureException(String message) {
-    super(String.format("%s: %s", ErrorCode.INFRASTRUCTURE_EXCEPTION, message));
+  public BidProcessingException(String message) {
+    super(String.format("%s: %s", ErrorCode.BID_PROCESSING_EXCEPTION, message));
   }
 
   public ErrorCode getErrorCode() {
--- src/main/java/com/example/bidder_service/domain/exception/ErrorCode.java
+++ src/main/java/com/example/bidder_service/domain/exception/ErrorCode.java
@@ -2,4 +2,5 @@ package com.example.bidder_service.domain.exception;
 
 public enum ErrorCode {
   INFRASTRUCTURE_EXCEPTION,
+  BID_PROCESSING_EXCEPTION,
 }
